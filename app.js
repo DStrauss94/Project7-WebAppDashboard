19009fd7e1ea0593071d5e42821bde571e972138
Douglas Strauss <douglasstrauss@Douglass-MacBook-Air.local>
2021-04-26 17:48:45 -0700

    Got the graph to show, but with no data populating yet

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const alertBanner = document .getElementById("alert");
+
+const alertBanner = document.getElementById("alert");
+const trafficCanvas = document.getElementById("traffic-chart");
+
 
 //create the html for the banner
 alertBanner.innerHTML = 
@@ -14,4 +17,36 @@ alertBanner.addEventListener('click', e => {
     }
 });
 
+let trafficData = {
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
+            "4-10", "11-7" , "18-24", "25-31"],
+            dataSets: [{
+                data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850
+                , 2250, 1500, 2500],
+                backgroundColor: 'rgba(116, 119, 191, .3)',
+                borderWidth: 1,
+            }]
+        };
 
+        let trafficOptions = {
+            aspectRatio: 2.5,
+            animation: {
+            duration: 0
+            },
+            scales: {
+                y:{
+                    beginAtZero : true
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false
+                }
+            }
+        };
+
+        let trafficChart = new Chart(trafficCanvas ,{
+            type: 'line',
+            data: trafficData,
+            options: trafficOptions
+        });
