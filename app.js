20dc5bc361cfe5de8ab9bf2a07895f265a7e67ad
Douglas Strauss <douglasstrauss94@gmail.com>
2021-05-13 20:31:29 -0700

    working on Recent Activity

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const trafficCanvas = document.getElementById("traffic-chart");
 //create the html for the banner
 alertBanner.innerHTML = 
 `<div class = "alert-banner" >
-    <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
+    <p><strong>Alert:</strong> You have unread messages </p>
     <p class= "alert-banner-close">x</p>
 </div> `
 
